3f999383c9535cf4d544368979c91eda1d3b16fc
maher <eidmaher09@example.com>
2026-09-03 03:18:20 +0300

    Add wedding wish box

--- send-wish.php
+++ send-wish.php
@@ -1,5 +1,13 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
